30a65db37582226fbe25dd480d5b36f1cdfe29a4
pp <phiphatphong_ka@cmu.ac.th>
2022-12-23 16:07:28 +0700

    Full

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main(){
+  string character;
+  int num;
+  int height;
+  int bounty;
+        cout << "Enter your age: ";
+        cin >> num;
+        if(num <= 25){
+            cout << "Enter your height: ";
+            cin >> height;
+            if(height < 100){
+                character = "Chopper";
+            }else if(height < 180){
+                character = "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if(bounty > 11*pow(10,8)){
+                    character = "Zoro";
+                }else{
+                    character = "Sanji";
+                }
+                }
+        }else if(num <= 60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 5*pow(10,8)){
+                character = "Jinbe";
+            }else{
+                character = "Franky";
+            }
+            
+        }else{
+            character = "Brook";
+        }
+        cout << "Your character = " << character ;
+   return 0;
+}
